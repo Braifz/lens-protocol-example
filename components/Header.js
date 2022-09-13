3962b8dec1e23388f777e05961ad363bcc674df1
Braifz <braian94lp@gmail.com>
2022-09-13 19:01:17 -0300

    create component Header

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import styles from "../styles/Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div>header</div>
+    </header>
+  );
+};
+
+export default Header;
