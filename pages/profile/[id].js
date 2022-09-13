b881ba9404691b6a3b0c4a7120e5ebc899413581
Braifz <braian94lp@gmail.com>
2022-09-13 14:35:21 -0300

    add publication fetch and show data publication

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { client, getProfileId } from "../../api";
+import { client, getProfileId, getPublications } from "../../api";
 import Image from "next/image";
 
 const Profile = () => {
   const [profile, setProfiles] = useState({});
+  const [pubs, setPubs] = useState([]);
   const router = useRouter();
   const { id } = router.query;
 
@@ -19,10 +20,19 @@ const Profile = () => {
       const response = await client.query(getProfileId, { id }).toPromise();
       console.log(response);
       setProfiles(response.data.profile);
+
+      const publicationData = await client
+        .query(getPublications, { id })
+        .toPromise();
+      setPubs(publicationData.data.publications.items);
+      console.log({ publicationData });
     } catch (e) {
       console.log(e);
     }
   };
+
+  console.log(pubs);
+  if (!profile) return null;
 
   return (
     <div
@@ -33,16 +43,43 @@ const Profile = () => {
         borderRadius: "8px",
       }}
     >
-      <h2>{id}</h2>
       <h1>{profile.handle}</h1>
       <h3>{profile.bio}</h3>
+      {profile.picture ? (
+        <Image
+          style={{
+            backgroundColor: "white",
+            borderRadius: "16px",
+          }}
+          src={profile.picture.original.url}
+          width="100px"
+          height="100px"
+          alt="img-profile"
+        />
+      ) : (
+        <div
+          style={{
+            width: "60px",
+            height: "60px",
+            backgroundColor: "white",
+            borderRadius: "16px",
+          }}
+        ></div>
+      )}
 
-      {/* <Image
-        src={profile.picture.original.url}
-        width="60px"
-        height="60px"
-        alt="img-profile"
-      /> */}
+      {/* <p>Followers: {profile.stats.totalFollowers}</p>
+      <p>Following: {profile.stats.totalFollowing}</p> */}
+      <h2>Publications</h2>
+      {pubs.length > 0 &&
+        pubs.map((pub, index) => (
+          <div style={{ padding: "16px" }} key={index}>
+            <h3>Content</h3>
+            <p>{pub.metadata.content}</p>
+            <h2>description</h2>
+            <p>{pub.metadata.description}</p>
+            <p>hello</p>
+          </div>
+        ))}
     </div>
   );
 };
