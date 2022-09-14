58e1add9d70fe995b46a6d94624288de5f3c0daa
Braifz <braian94lp@gmail.com>
2022-09-14 11:07:51 -0300

    create component cardprofile

---
+++ components/CardProfile.js
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const CardProfile = ({ profile }) => {
+  console.log(profile);
+  return (
+    <Link href={`/profile/${profile.id}`} key={profile.id}>
+      <a>
+        <div className="border-green-300 rounded-lg border-2 m-5 p-5 max-w-screen-sm">
+          <h3 className="text-2xl font-semibold ">{profile.name}</h3>
+          <h2>{profile.handle}</h2>
+          <p>{profile.bio}</p>
+          <h3>{profile.ownedBy}</h3>
+          {/* {profile.picture ? (
+            <Image
+              src={profile.picture.original.url}
+              width="60px"
+              height="60px"
+              alt="img-profile"
+            />
+          ) : (
+            <div
+              style={{
+                width: "60px",
+                height: "60px",
+                backgroundColor: "white",
+                borderRadius: "16px",
+              }}
+            ></div>
+          )} */}
+        </div>
+      </a>
+    </Link>
+  );
+};
+
+export default CardProfile;
