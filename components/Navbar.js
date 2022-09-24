be8b73e02a8a149b55c09a068d4122a7844c8245
Braifz <braian94lp@gmail.com>
2022-09-24 15:19:10 -0300

    linter

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
 import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
